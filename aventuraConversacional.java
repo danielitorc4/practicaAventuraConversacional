55ce61d255c8bd310e450dc4aa931c1923a40781
danielitorc4 <danielitorcc4@gmail.com>
2024-10-29 22:42:12 +0100

    aventuraConversacional 1.0
    String Mundo

---
+++ aventuraConversacional.java
@@ -0,0 +1,18 @@
+package dam.islasfilipinas.practicaAventuraConversacional;
+
+import java.util.Scanner;
+
+public class aventuraConversacional {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String[][][] mundo = new String[15][15][3];
+
+		scan.close();
+	}
+
+	public static void moverse {
+		
+	}
+	
+}
